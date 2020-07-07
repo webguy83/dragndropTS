b1e896d3e51c48ad4f08be7da94e2bf32ef77501
Curtis Yacboski <curtisyacboski@gmail.com>
2020-07-06 20:08:56 -0700

    progressing

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,16 @@
 // Code goes here!
-const formTemplateEl = document.getElementById(
-  'project-input'
-) as HTMLTemplateElement;
+const formTemplateEl = document.getElementById('project-input') as HTMLTemplateElement;
+const projectListTemplateEl = document.getElementById('project-list') as HTMLTemplateElement;
+const singleProjectTemplateEl = document.getElementById('single-project') as HTMLTemplateElement;
+const formEl = formTemplateEl.firstChild as HTMLFormElement;
 const appEl = document.getElementById('app') as HTMLDivElement;
 
-const titleEl = document.getElementById('title') as HTMLInputElement;
-const descriptionEl = document.getElementById(
-  'description'
-) as HTMLTextAreaElement;
-const peopleEl = document.getElementById('people') as HTMLInputElement;
 
 interface IUserData {
   title: string;
   description: string;
   people: number;
 }
-
-const data: IUserData = {
-  title: '',
-  description: '',
-  people: 0,
-};
-
-function onSubmit() {}
 
 function WithTemplate(tmp: HTMLTemplateElement, hookId: HTMLElement) {
   return function (_: any) {
@@ -33,12 +21,69 @@ function WithTemplate(tmp: HTMLTemplateElement, hookId: HTMLElement) {
 
 @WithTemplate(formTemplateEl, appEl)
 class CustomForm {
-  constructor(public temp: number) {}
+  private customFormEl: HTMLFormElement;
+  private titleEl: HTMLInputElement;
+  private descriptionEl: HTMLTextAreaElement;
+  private peopleEl: HTMLInputElement;
 
-  run() {
-    console.log(this.temp);
+  constructor() {
+   this.customFormEl = document.getElementById('user-input') as HTMLFormElement;
+   this.titleEl = document.getElementById('title') as HTMLInputElement;
+   this.descriptionEl = document.getElementById('description') as HTMLTextAreaElement;
+   this.peopleEl = document.getElementById('people') as HTMLInputElement;
+   this.attachListeners();
+  }
+
+  attachListeners() {
+    this.customFormEl.addEventListener("submit", this.onSubmit.bind(this));
+  }
+
+  onSubmit(event:Event) {
+    event.preventDefault();
+    const title = this.titleEl.value;
+    const description = this.descriptionEl.value;
+    const people = +this.peopleEl.value;
+    const project: IUserData = {
+      title, description, people
+    }
+    pl.add(project);
   }
 }
 
-const f = new CustomForm(3);
-f.run();
+@WithTemplate(projectListTemplateEl, appEl)
+class ProjectList {
+  private projects: IUserData[] = [];
+
+  add(project: IUserData) {
+    this.projects.push(project);
+    this.refresh();
+  }
+
+  refresh() {
+    const projectPlacementElm = document.querySelector('.projects ul') as HTMLUListElement;
+    projectPlacementElm.appendChild(document.createElement('li'));
+      console.log(projectPlacementElm.children)
+    this.projects.forEach((_proj, _i) => {
+      
+      // projectPlacementElm.children[0].textContent = proj.title;
+      // projectPlacementElm.appendChild(projectPlacementElm.children[0]);
+      // const sp = new SingleProject();
+      // sp.addContent(proj);
+      
+    });
+    // console.log(this.projects)
+  }
+}
+
+// const projectPlacementElm = document.querySelector('.projects ul') as HTMLUListElement;
+// @WithTemplate(singleProjectTemplateEl, projectPlacementElm)
+// class SingleProject {
+//   addContent(project: IUserData) {
+//     console.log(projectPlacementElm.children);
+//     projectPlacementElm.children[0].textContent = project.title;
+//     projectPlacementElm.appendChild(projectPlacementElm.children[0]);
+//   }
+// }
+
+const cf = new CustomForm();
+const pl = new ProjectList();
